5e08a17815b03e7a0918f9d2e65f3ba2457b783c
Praktikumsbenutzer <sikprak@sik-praktikum11.informatik.uni-augsburg.de>
2013-06-18 16:42:25 +0200

    modified get_angle() function again

--- src/program.d
+++ src/program.d
@@ -1,3 +1,4 @@
 program.o: src/program.c include/program.h include/board.h \
   include/nios2s-db_4ce10.h include/led.h include/board.h include/pio.h \
-  include/roomba.h include/uart.h include/tools.h include/mymath.h
+  include/roomba.h include/uart.h include/tools.h include/mymath.h \
+  include/workbench.h include/mymath.h
